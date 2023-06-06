f6aaba6263280a5939ce9a25130f869636daf026
Samuel Granvik <samuelgranvik@Samuels-MacBook-Pro.local>
2023-06-06 13:57:06 +0300

    Initial setup

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,14 +6,26 @@
       </ion-toolbar>
     </ion-header>
 
-
+    <ion-fab>
+      <ion-fab-button>
+        <ion-icon :icon="camera"></ion-icon>
+      </ion-fab-button>
+    </ion-fab>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  import { 
+    IonContent, 
+    IonHeader, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar,
+    IonIcon,
+    IonFab,
+    IonFabButton
+  } from '@ionic/vue';
+  import { camera } from 'ionicons/icons';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
